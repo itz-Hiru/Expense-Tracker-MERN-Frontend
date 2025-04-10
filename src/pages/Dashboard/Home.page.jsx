f038211076fad7abb440cfff66a14930c4819d1c
itz-Hiru <hirumithagithub@gmail.com>
2025-04-10 14:46:57 +0530

    Created Dashboard Info Cards

--- src/pages/Dashboard/Home.page.jsx
+++ src/pages/Dashboard/Home.page.jsx
@@ -1,16 +1,71 @@
-import React from 'react'
-import DashboardLayout from "../../components/layouts/DashboardLayout.layout"
-import { useUserAuth } from '../../hooks/useUserAuth.hook';
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useUserAuth } from "../../hooks/useUserAuth.hook";
+import axiosInstance from "../../utils/axiosInstance.util";
+import { API_PATHS } from "../../utils/apiPath.util";
+import { addThousandsSeparator } from "../../utils/helper.util";
+import DashboardLayout from "../../components/layouts/DashboardLayout.layout";
+import InfoCard from "../../components/cards/InfoCard.card";
+import { LuHandCoins, LuWalletMinimal } from "react-icons/lu";
+import { IoMdCard } from "react-icons/io";
 
 const Home = () => {
   useUserAuth();
+  const navigate = useNavigate();
+
+  const [dashboardData, setDashboardData] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const fetchDashboardData = async () => {
+    if (loading) return;
+    setLoading(true);
+
+    try {
+      const response = await axiosInstance.get(
+        `${API_PATHS.DASHBOARD.GET_STATS}`
+      );
+
+      if (response.data) {
+        setDashboardData(response.data);
+      }
+    } catch (error) {
+      console.log("Something went wrong. Please try again! ", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchDashboardData();
+    return () => {};
+  }, []);
+
   return (
     <DashboardLayout activeMenu="Dashboard">
       <div className="my-5 mx-auto">
-
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+          <InfoCard
+            icon={<IoMdCard />}
+            label="Total Balance"
+            value={addThousandsSeparator(dashboardData?.totalBalance || 0)}
+            color="bg-primary"
+          />
+          <InfoCard
+            icon={<LuWalletMinimal />}
+            label="Total Income"
+            value={addThousandsSeparator(dashboardData?.totalIncomes || 0)}
+            color="bg-orange-500"
+          />
+          <InfoCard
+            icon={<LuHandCoins />}
+            label="Total Expense"
+            value={addThousandsSeparator(dashboardData?.totalExpenses || 0)}
+            color="bg-red-500"
+          />
+        </div>
       </div>
     </DashboardLayout>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- src/utils/helper.util.js
+++ src/utils/helper.util.js
@@ -18,3 +18,15 @@ export const getInitials = (name) => {
   }
   return initials.toUpperCase();
 };
+
+export const addThousandsSeparator = (num) => {
+  const number = Number(num);
+  if (num == null || isNaN(number)) return "";
+
+  const [integerPart, fractionalPart] = number.toString().split(".");
+  const formattedInteger = integerPart.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+
+  return fractionalPart
+    ? `${formattedInteger}.${fractionalPart}`
+    : formattedInteger;
+};
